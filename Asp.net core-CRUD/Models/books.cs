0a8e38e25cd9ea62e9842ce6a07dc882c6cc0e67
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-01-29 11:59:17 +0100

    add books class Employee

---
+++ Asp.net core-CRUD/Models/books.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Asp.net_core_CRUD.Models
+{
+    public class books
+    {
+        public int ID { get; set; }
+        [MinLength(3)]
+        [MaxLength(150)]
+        [Required]
+        public string Title { get; set; } = "";
+        public string? Language { get; set; }
+        [MinLength(3)]
+        [MaxLength(150)]
+        public string Author { get; set; } = "";
+        public DateTime PublishDate { get; set; }
+        public int AvailableCopies { get; set; }
+    }
+}
